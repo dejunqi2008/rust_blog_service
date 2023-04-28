f5f71effe0d59c4bfa8f654d587cb3bf85de6815
dejunqi <dejunqi2008@gmail.com>
2023-04-27 22:12:02 -0700

    connect to remote mysql db

--- src/api/tag.rs
+++ src/api/tag.rs
@@ -19,38 +19,38 @@ pub async fn get_tags(
     db :Data<R2D2Pool<MySqlConnectionManager>>
 ) -> Result<Json<Vec<Tag>>, GenericError> {
 
-    // let mut conn: PooledConnection<MySqlConnectionManager> = db.get().unwrap();
-    // let sql_str = "SELECT * FROM tags".to_string();
+    let mut conn: PooledConnection<MySqlConnectionManager> = db.get().unwrap();
+    let sql_str = "SELECT * FROM tags".to_string();
 
-    // let res = conn.query_map( sql_str, |(id, tagname, description)| Tag {
-    //     id,
-    //     tagname,
-    //     description
-    // });
+    let res = conn.query_map( sql_str, |(id, tagname, description)| Tag {
+        id,
+        tagname,
+        description
+    });
 
-    // return match res {
-    //     Ok(item) => Ok(Json(item)),
-    //     Err(e) => {
-    //         // TODO: better handle different kind of error
-    //         println!("{:?}", e.to_string());
-    //         Err(GenericError::UnknowError)
-    //     }
-    // };
+    return match res {
+        Ok(item) => Ok(Json(item)),
+        Err(e) => {
+            // TODO: better handle different kind of error
+            println!("{:?}", e.to_string());
+            Err(GenericError::UnknowError)
+        }
+    };
 
     // Use dummy data before setting MYSQL on AWS
-    let t1 = Tag {
-        id: 1,
-        tagname: "sport".to_string(),
-        description: None
-    };
-    let t2 = Tag {
-        id: 2,
-        tagname: "economics".to_string(),
-        description: Some("Topics about economic".to_string())
-    };
+    // let t1 = Tag {
+    //     id: 1,
+    //     tagname: "sport".to_string(),
+    //     description: None
+    // };
+    // let t2 = Tag {
+    //     id: 2,
+    //     tagname: "economics".to_string(),
+    //     description: Some("Topics about economic".to_string())
+    // };
 
-    let mut res: Vec<Tag> = vec![];
-    res.push(t1);
-    res.push(t2);
-    return Ok(Json(res));
+    // let mut res: Vec<Tag> = vec![];
+    // res.push(t1);
+    // res.push(t2);
+    // return Ok(Json(res));
 }
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,58 @@ mod api;
 mod model;
 mod util;
 
+use std::sync::{Mutex, Arc};
+
 use mysql::{prelude::*, Opts, OptsBuilder};
 
 use actix_web::{web, App, HttpServer};
 use r2d2::{Pool as R2D2Pool};
 use r2d2_mysql::MySqlConnectionManager;
 use crate::api::tag::get_tags;
+use std::env;
+use dotenv::dotenv;
+use std::fs;
+
 
 fn create_db_pool() -> R2D2Pool<MySqlConnectionManager> {
-    let url = "mysql://root:dejunqilocal@localhost:3306/myblog";
-    let ops = Opts::from_url(&url).unwrap();
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not found");
+    let ops = Opts::from_url(&database_url).unwrap();
     let builder = OptsBuilder::from_opts(ops);
     let manager = MySqlConnectionManager::new(builder);
     R2D2Pool::builder()
-        .max_size(20)
+        .max_size(5)
         .build(manager)
         .unwrap()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
     println!("Server is running on part: 3000");
     let pool: R2D2Pool<MySqlConnectionManager> = create_db_pool();
 
+    let mut host_url = "".to_owned();
+    let content = fs::read_to_string(".host");
+    match content {
+        Err(e) => {
+            println!("{:?}", e);
+            host_url = "127.0.0.1".to_owned();
+        },
+        Ok(s) => {
+            host_url = s;
+        }
+    }
+    let mut url = host_url.trim_end_matches(&['\r', '\n'][..]).to_string();
+    url.push_str(":3000");
+    println!("{}", url);
     return HttpServer::new(move || {
             App::new()
             .app_data(web::Data::new(pool.clone()))
                 .service(get_tags)
             })
-            .bind("127.0.0.1:3000")?
+            .bind(url)?
             .run()
             .await;
 }
